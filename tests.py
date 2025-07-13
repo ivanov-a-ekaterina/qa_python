ae8adead585047571a061932e2e150c3d1144d60
Ekaterina Ivanova <ivanov-a-ekaterina@yandex.ru>
2025-07-13 14:09:46 +0300

    new_commit_1

--- tests.py
+++ tests.py
@@ -22,3 +22,59 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_init(self):
+        collector = BooksCollector
+        assert collector.books_genre == {}
+        assert collector.favorites == []
+        assert collector.genre == ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
+        assert collector.genre_age_rating == ['Ужасы', 'Детективы']
+
+    @pytest.mark.parametrize('name', ['!№;', 'Книга1 Книга1 Книга1 Книга1 Книга1 Книга1'])
+    def test_add_new_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('name')
+        assert 'name' not in collector.books_genre
+
+    def test_set_book_genre(self):
+        collector = BooksCollector()
+        name = 'Книга1'
+        genre = 'Комедии'
+        collector.set_book_genre(name, genre)
+        assert name and genre in self.collector.books_genre
+
+    def test_get_book_genre(self):
+        collector = BooksCollector()
+        name = 'Книга1'
+        genre = 'Комедии'
+        collector.set_book_genre(name, genre)
+        assert name and genre in self.collector.books_genre
+
+    def test_get_book_with_specific_genre(self):
+        name1 = 'Книга1'
+        name2 = 'Книга2'
+        genre = 'Комедии'
+        self.collector.set_book_genre(name1, genre)
+        self.collector.set_book_genre(name2, genre)
+        result = self.collector.get_book_with_specific_genre(genre)
+        assert result, [name1, name2]
+
+    def test_add_book_in_favourite(self):
+        collector = BooksCollector()
+        collector.add_new_book('Книга1')
+        collector.add_new_book_in_favourite('Книга1')
+        assert 'Книга1' in collector.get_list_of_favourite_books()
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Книга1')
+        collector.add_new_book_in_favourite('Книга1')
+        collector.delete_book_from_favourite('Книга1')
+        assert 'Книга1' not in collector.get_list_of_favourite_books()
+
+    def test_get_list_of_favourites(self):
+        collector = BooksCollector()
+        books = ['Книга1', 'Книга2', 'Книга3']
+        for book in books:
+            collector.add_new_book(book)
+            collector.add_book_in_favourites(book)
+        assert collector.get_list_of_favourites() == books
